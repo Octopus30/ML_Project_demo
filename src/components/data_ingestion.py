9cd524aa806b2b9a98cc051d5ebf5da4c360c36e
Octopus30 <akhil.reddy301211996@gmail.com>
2023-11-09 17:37:21 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,13 @@
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import Modeltrainer,ModelTrainerConfig
 
 import os
 import sys
 import pandas as pd
+import numpy as np
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -54,5 +57,7 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
     #this is the path of train and test data
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
     
+    modeltrainer = Modeltrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.logger import logging
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -17,3 +18,30 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def evaluate_models(X_train,Y_train,X_test,Y_test,models):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            
+            model.fit(X_train,Y_train)
+            
+            Y_train_pred = model.predict(X_train)
+            
+            Y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(Y_train,Y_train_pred)
+            
+            test_model_score = r2_score(Y_test,Y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
+            
+            
+    except Exception as e:
+        raise CustomException(e,sys)
+        
