46f85bf929ca7b8a9ffdc52ae427dc761057f68d
Octopus30 <akhil.reddy301211996@gmail.com>
2023-11-09 12:32:21 +0530

    DataTransformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 import os
 import sys
@@ -50,4 +51,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    #this is the path of train and test data
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
